12afe2b985259d668152feef475963a9b8d52eb8
armand-meskin <armandm101@gmail.com>
2024-02-15 11:40:10 -0500

    Completed

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,14 @@
-// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. z
+
+const logger = {
+    logMsg: function() {
+        console.log(`Error Message: ${this.errMsg}`);
+    }
+}
+
+const err = {
+    errMsg:"This is an error!"
+}
+
+logger.logMsg.call(err);
+
